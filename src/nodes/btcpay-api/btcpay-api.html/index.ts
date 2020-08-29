435dfecc697e7f7c98ab0bca910b93e8837ea6ef
Alex Kaul <gh@alexkaul.com>
2020-08-29 11:45:24 +0700

    Improve path editor

--- src/nodes/btcpay-api/btcpay-api.html/index.ts
+++ src/nodes/btcpay-api/btcpay-api.html/index.ts
@@ -8,7 +8,7 @@ RED.nodes.registerType<BtcpayApiEditorNodeProperties>("btcpay-api", {
   color: "#a6bbcf",
   defaults: {
     method: { value: "GET" },
-    path: { value: "" },
+    path: { value: "", validate: (val) => val.length > 1 },
     client: { value: "", type: "btcpay-api-config", required: true },
     name: { value: "" },
   },
@@ -18,5 +18,15 @@ RED.nodes.registerType<BtcpayApiEditorNodeProperties>("btcpay-api", {
   paletteLabel: "btcpay api",
   label: function () {
     return this.name || "btcpay api";
+  },
+  oneditsave: function () {
+    let val = $("#node-input-path").val()?.toString() || "";
+    val = val.trim();
+    if (val.length > 0) {
+      if (val.substr(0, 1) !== "/") {
+        val = "/" + val;
+      }
+    }
+    $("#node-input-path").val(val);
   },
 });
